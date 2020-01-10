8c017e85e2aec32f567bc86563ba7bfb47c80d53
Dobfek <dobfek@hotmail.com>
2020-01-10 23:15:28 +0100

    Update rpi-edid-lock.sh
    Updated to work with LibreELEC! Tested with RPi 3B + LibreELEC 7.0.3

--- rpi-edid-lock.sh
+++ rpi-edid-lock.sh
@@ -1,6 +1,6 @@
 #/usr/bin/env bash
 
-FAT_PARTITION='/boot'
+FAT_PARTITION='/flash'
 CONFIG_FILE='config.txt'
 
 # run <command> <error_message>
@@ -15,23 +15,26 @@ run() {
 replace_line() {
     if [ -n "$3" ]; then
         if grep -q "$2" "$1"; then
-            sudo sed -i "/$2/ c\\$3" "$1"
+            sed -i "/$2/ c\\$3" "$1"
         else
-            echo "$3" | sudo tee -a "$1" >/dev/null
+            echo "$3" | tee -a "$1" >/dev/null
         fi
     else
-        sudo sed -i "/$2/d" "$1"
+        sed -i "/$2/d" "$1"
     fi
 }
 
 # check access rights
-run "sudo -n uptime" "You need to be root to perform this command!"
+# run "sudo -n uptime" "You need to be root to perform this command!"
+
+# remount /flash as rw for LibreELEC 7.0.3
+run "mount -o remount,rw /dev/mmcblk0p1 /$FAT_PARTITION" "Could not remount"
 
 # cd to FAT partition
 run "cd $FAT_PARTITION" "Can't find FAT partition!"
 
 # save EDID data to file
-run "sudo tvservice -d edid.dat" "Can't find tvservice utility!"
+run "tvservice -d edid.dat" "Can't find tvservice utility!"
 
 # check if config.txt file exists
 if ! [ -f "$CONFIG_FILE" ]; then
@@ -56,5 +59,6 @@ while getopts ":lu" opt; do
     esac
 done
 
+echo "Running without parameters WILL modify the edid.dat too!! "
 echo "usage: $0 [-l|-u]"
 exit 1
